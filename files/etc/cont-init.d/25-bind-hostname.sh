4710b23edd9b90df8850d0c51053d8cad2d7d87c
PJ Eby <pje@telecommunity.com>
2020-03-02 23:24:45 -0500

    Fix outbound mail forwards/redirects from dovecot

--- files/etc/cont-init.d/25-bind-hostname.sh
+++ files/etc/cont-init.d/25-bind-hostname.sh
@@ -7,6 +7,7 @@ bindhost=$(hostname)
 sed -i 's/__HOST__/'"$bindhost"/                                        /etc/nginx/sites-enabled/administration
 sed -i 's/^listen=.*:25$/listen='"$bindhost/"                           /opt/haraka-smtp/config/smtp.ini
 sed -i 's/^listen=.*:587,.*:465$/listen='"$bindhost:587,$bindhost:465/" /opt/haraka-submission/config/smtp.ini
+sed -i 's/submission_host = .*:587$/submission_host = '"$bindhost:587/" /etc/dovecot/conf.d/15-lda.conf
 
 # Haraka should only do outbound connects on our IP
 hostname -i >/opt/haraka-submission/config/my-ip
